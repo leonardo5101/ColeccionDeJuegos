87ce10249223ef1b0206a6e9f41f1f1b8798ef52
leonardo5101 <tumamaconpipilin>
2018-04-30 12:05:21 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Jhordan Mayhua on 30/04/18.
+//  Copyright © 2018 Jhordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
